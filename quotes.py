a2a8664d202dba81bf290617e226427eaf15512a
arunkhattri <arunkhattri@outlook.com>
2024-02-18 22:59:38 +0530

    follow next page

--- quotes.py
+++ quotes.py
@@ -10,10 +10,15 @@ class QuotesSpider(scrapy.Spider):
         self.log("i just visited: " + response.url)
         for quote in response.xpath("//div[@class='quote']"):
             item = {
-                'author': quote.xpath(".//small[@class='author']/text()")
-                .extract_first(),
-                'text': quote.xpath(".//span[@class='text']/text()").
-                extract_first(),
-                'tags': quote.xpath('.//a[@class="tag"]/text()').extract(),
+                "author": quote.xpath(
+                    ".//small[@class='author']/text()"
+                ).extract_first(),
+                "text": quote.xpath(".//span[@class='text']/text()").extract_first(),
+                "tags": quote.xpath('.//a[@class="tag"]/text()').extract(),
             }
             yield item
+        # follow pagination link
+        next_page_url = response.xpath("//li[@class='next']/a/@href").extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse)
